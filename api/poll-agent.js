249ff7d39982c269762cdefb9c380a571623483e
Chris Morin <127409240+Chris-Morin@users.noreply.github.com>
2026-04-12 19:35:10 +0800

    security: add requireAdmin to poll-agent

--- api/poll-agent.js
+++ api/poll-agent.js
@@ -7,10 +7,15 @@
  * GET ?task_id=xxx
  */
 
+var auth = require('./_lib/auth');
 module.exports = async function(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
+
+  // Require authenticated admin
+  var user = await auth.requireAdmin(req, res);
+  if (!user) return;
 
   var AGENT_URL = process.env.AGENT_SERVICE_URL;
   var AGENT_KEY = process.env.AGENT_API_KEY;
